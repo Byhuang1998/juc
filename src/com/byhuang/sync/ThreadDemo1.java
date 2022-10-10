f7b0b33809fca66523e58f34ba39ac71c6cbf91f
mskj-huangbingyi <byhuang1998@gmail.com>
2022-10-10 20:07:25 +0800

    master juc two threads alternately print 1 0

---
+++ src/com/byhuang/sync/ThreadDemo1.java
@@ -0,0 +1,56 @@
+package com.byhuang.sync;
+
+/**
+ * @author mskj-huangbingyi
+ * @version 1.0
+ * @data 2022/10/10 19:56
+ * @description TODO
+ */
+public class ThreadDemo1 {
+
+    public static void main(String[] args) {
+        Share share = new Share();
+
+        new Thread(() -> {
+            for (int i = 0; i <= 10; i++) {
+                try {
+                    share.add();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "A").start();
+        new Thread(() -> {
+            for (int i = 0; i <= 10; i++) {
+                try {
+                    share.sub();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "B").start();
+    }
+}
+
+class Share {
+    private int number = 0;
+
+    public synchronized void add() throws InterruptedException {
+        if (number != 0) {
+            this.wait();
+        }
+        number++;
+        System.out.println(Thread.currentThread().getName() + ":" + number);
+        this.notifyAll();
+    }
+
+    public synchronized void sub() throws InterruptedException {
+
+        if (number != 1) {
+            this.wait();
+        }
+        number--;
+        System.out.println(Thread.currentThread().getName() + ":" + number);
+        this.notifyAll();
+    }
+}
