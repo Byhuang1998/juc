dbc8b1c9a7c377276c77e0992fcca313a351bc2a
mskj-huangbingyi <byhuang1998@gmail.com>
2022-10-12 10:26:22 +0800

    show the problem of Map thread-unsafe

---
+++ src/com/byhuang/lock/ThreadDemo6.java
@@ -0,0 +1,27 @@
+package com.byhuang.lock;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * @author mskj-huangbingyi
+ * @version 1.0
+ * @date 2022/10/12 9:51
+ * @description list线程不安全问题
+ * java.util.ConcurrentModificationException
+ */
+public class ThreadDemo6 {
+
+    public static void main(String[] args) {
+
+        Map<Integer, String> map = new HashMap<>();
+        for (int i = 0; i < 30; i++) {
+            int finalI = i;
+            new Thread(() -> {
+                map.put(finalI, UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(map);
+            }, "i").start();
+        }
+    }
+}
