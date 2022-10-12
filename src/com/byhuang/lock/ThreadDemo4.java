4b9fc7ed78511ceb845fe7fa3aada8d4d4551ee7
mskj-huangbingyi <byhuang1998@gmail.com>
2022-10-12 10:11:08 +0800

    solve the problem of List thread-unsafe

--- src/com/byhuang/lock/ThreadDemo4.java
+++ src/com/byhuang/lock/ThreadDemo4.java
@@ -1,8 +1,7 @@
 package com.byhuang.lock;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * @author mskj-huangbingyi
@@ -15,8 +14,16 @@ public class ThreadDemo4 {
 
     public static void main(String[] args) {
 
-        List<String> list = new ArrayList<>();
+//        List<String> list = new ArrayList<>();
 
+        // solution 1
+//        List<String> list = new Vector<>();
+
+        // solution 2
+//        List<String> list = Collections.synchronizedList(new ArrayList<>());
+
+        // solution 3 写时复制技术 Lock加锁，复制一份原List后，添加元素，覆盖原List
+        List<String> list = new CopyOnWriteArrayList<>();
         for (int i = 0; i < 30; i++) {
             new Thread(() -> {
                 list.add(UUID.randomUUID().toString().substring(0, 8));
